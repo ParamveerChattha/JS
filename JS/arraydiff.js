5d9eab546a4d713626cbb3ebd99a082a8555bffa
ParamveerChattha <paramveersingh08.92@gmail.com>
2017-08-18 14:50:30 +0530

    uptill 6

--- JS/arraydiff.js
+++ JS/arraydiff.js
@@ -2,9 +2,23 @@ var arrays = function(arr1,arr2){
 
   this.arr1=arr1;
   this.arr2=arr2;
-  console.log(this.arr1 + ' ' + this.arr2)
+  //console.log(this.arr1 + ' ' + this.arr2)
+
 
 }
 var arr1= [3,5,8,5,9]
 var arr2=[4,6,9,4,0,3,1]
-var arraycall = new arrays()
+var arraycall = new arrays(arr1,arr2);
+
+arrays.prototype.arraydiff=function(){
+    var set1=this.arr1
+    var set2=this.arr2
+    console.log( set1 + ' - - ' + set2);
+    var set_1=new Set(set1);
+    var set_2=new Set(set2);
+    console.log('sets are' + ' ' + set_1 + '  ' + set_2);
+//    var arrayU= _.union(arr1,arr2);
+
+}
+
+arraycall.arraydiff();
