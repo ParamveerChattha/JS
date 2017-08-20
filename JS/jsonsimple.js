8523b95e8cee9a7b40c5a45f3c993a6f50a3e248
ParamveerChattha <paramveersingh08.92@gmail.com>
2017-08-20 11:15:51 +0530

    object sorting done-q-7

--- JS/jsonsimple.js
+++ JS/jsonsimple.js
@@ -7,11 +7,39 @@ var library = [
 //var library = JSON.stringify(library);
 //var libstring = (JSON.parse(libobj));
 //console.log(libstring);
-console.log(`---------------------------------------------------------`);
+//console.log(`---------------------------------------------------------`);
 //console.log(libobj);
-console.log(library.title);
-console.log(library.length);
+//console.log(library.title);
+//console.log(library.length);
 //console.log(libstring['title']);
+var title=[];
+var arr=[];
 for(i=0; i<library.length;i++){
-  console.log(library[i].title);
-}
+  //console.log(library[i].title);
+  title.push(library[i].title);
+  }
+  title.sort();
+  console.log(title);
+//  console.log(title);
+    for(var i in library){
+      for (var j in library){
+      if(library[j].title===title[i]){
+        arr.push(library[j]);
+      }
+    }
+  }
+console.log(arr);
+//
+//
+// function cs(x,y)
+//  {
+//   if (x.author < y.author ){
+//     console.log(x.author + ' ' + y.author) ;
+//     return -1;
+//   }
+//   if (x.author > y.author)
+//     return 1;
+//   return 0;
+//  }
+//
+// console.log(library.sort(cs));
