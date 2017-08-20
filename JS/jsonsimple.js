60b0ce7e8aeb9e8089e3c7d8e4207556eeea759a
ParamveerChattha <paramveersingh08.92@gmail.com>
2017-08-20 09:51:56 +0530

    title exracted ques 7

--- JS/jsonsimple.js
+++ JS/jsonsimple.js
@@ -1,13 +1,17 @@
 var library = [
-{ author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
-{ author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
-{ author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+  { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+  { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+  { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
 ];
 
-var libobj = JSON.stringify(library);
-var libstring = (JSON.parse(libobj));
-console.log(libstring);
+//var library = JSON.stringify(library);
+//var libstring = (JSON.parse(libobj));
+//console.log(libstring);
 console.log(`---------------------------------------------------------`);
-console.log(libobj);
-console.log(libobj.title);
-console.log(libstring.title);
+//console.log(libobj);
+console.log(library.title);
+console.log(library.length);
+//console.log(libstring['title']);
+for(i=0; i<library.length;i++){
+  console.log(library[i].title);
+}
